e0efa095bba53ab42c3989924243330865fcbe3d
Roman Deriga <deriga.roman@gmail.com>
2023-08-10 14:23:57 +0300

    Create function shorten_link

--- main.py
+++ main.py
@@ -7,24 +7,27 @@ import json
 BITLY_ACCESS_TOKEN = dotenv_values('.env')['BITLY_ACCESS_TOKEN']
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-    get_user_url = 'https://api-ssl.bitly.com/v4/user'
+def shorten_link(token, url):
     get_bitly_link = 'https://api-ssl.bitly.com/v4/bitlinks'
+    
     headers = {
         'Authorization': f'Bearer {BITLY_ACCESS_TOKEN}',
     }
-
+    
     payload = {
-        'long_url': 'http://google.com'
+        'long_url': f'{url}'
     }
-
     
     response = requests.post(get_bitly_link, headers=headers, json=payload)
     response.raise_for_status()
-    #logging.info(response.url)
-    short_link = response.json()['link']
-    print(short_link)
+    return response.json()['link']
+
+
+def main():
+    get_user_url = 'https://api-ssl.bitly.com/v4/user'
+    long_url = 'http://google.com'
+    
+    print(shorten_link(BITLY_ACCESS_TOKEN, long_url))
     #logging.info(response)
     #pprint.pprint(response.json())
 
