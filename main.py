15999860faa62cdd1057798f1908bacd93ed6410
Roman Deriga <deriga.roman@gmail.com>
2023-08-12 18:52:13 +0700

    Add fixes

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from dotenv import dotenv_values
 import requests
 
 
-
 def is_bitlink(token, url):
-    check_bitlink_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    bitly_checking_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
     parsed_url = urlparse(url)
-    url = f'{check_bitlink_url}{parsed_url.netloc}{parsed_url.path}'
+    url = f'{bitly_checking_url}{parsed_url.netloc}{parsed_url.path}'
     headers = {
         'Authorization': f'Bearer {token}',
     }
@@ -17,17 +16,17 @@ def is_bitlink(token, url):
 
 
 def shorten_link(token, url):
-    get_bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+    bitly_shorting_url = 'https://api-ssl.bitly.com/v4/bitlinks'
 
     headers = {
         'Authorization': f'Bearer {token}',
     }
 
     payload = {
-        'long_url': f'{url}'
+        'long_url': url
     }
-    
-    response = requests.post(get_bitly_url, headers=headers, json=payload)
+
+    response = requests.post(bitly_shorting_url, headers=headers, json=payload)
     response.raise_for_status()
     bitlink = response.json()['link']
     return bitlink
@@ -39,16 +38,17 @@ def clicks_count(token, url):
     }
     parsed_url = urlparse(url)
     bitlink = f'{parsed_url.netloc}{parsed_url.path}'
-    get_clicks_count_url = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'.format(bitlink=bitlink)
+    bitly_counting_url = f'''https://api-ssl.bitly.com\
+        /v4/bitlinks/{bitlink}/clicks/summary'''.replace(' ', '')
 
-    response = requests.get(get_clicks_count_url, headers=headers)
+    response = requests.get(bitly_counting_url, headers=headers)
     response.raise_for_status()
     clicks = response.json()['total_clicks']
     return clicks
 
 
 def main():
-    url = input('Введите ссылку: ')
+    url = 'https://bit.ly/3s4R0At'
     token = dotenv_values('.env')['BITLY_ACCESS_TOKEN']
 
     if is_bitlink(token, url):
