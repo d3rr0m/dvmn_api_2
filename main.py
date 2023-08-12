8336425206e09fd70b76e9dff72e35bd5b8fb9b2
Roman Deriga <deriga.roman@gmail.com>
2023-08-12 11:56:29 +0700

    Add function is_bitlink

--- main.py
+++ main.py
@@ -1,83 +1,66 @@
 from urllib.parse import urlparse
 from dotenv import dotenv_values
-import logging
 import requests
-import pprint
 
 
-BITLY_ACCESS_TOKEN = dotenv_values('.env')['BITLY_ACCESS_TOKEN']
+
+def is_bitlink(token, url):
+    check_bitlink_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    parsed_url = urlparse(url)
+    url = f'{check_bitlink_url}{parsed_url.netloc}{parsed_url.path}'
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.ok
 
 
 def shorten_link(token, url):
-    get_bitly_link = 'https://api-ssl.bitly.com/v4/bitlinks'
-    
+    get_bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+
     headers = {
-        'Authorization': f'Bearer {BITLY_ACCESS_TOKEN}',
+        'Authorization': f'Bearer {token}',
     }
-    
+
     payload = {
         'long_url': f'{url}'
     }
     
-    response = requests.post(get_bitly_link, headers=headers, json=payload)
-    logging.info(response.status_code)
+    response = requests.post(get_bitly_url, headers=headers, json=payload)
     response.raise_for_status()
     bitlink = response.json()['link']
     return bitlink
 
 
 def clicks_count(token, url):
-    get_bitly_link = 'https://api-ssl.bitly.com/v4/bitlinks'
-        
     headers = {
-        'Authorization': f'Bearer {BITLY_ACCESS_TOKEN}',
+        'Authorization': f'Bearer {token}',
     }
-    
-    payload = {
-        'long_url': f'{url}'
-    }
-    
-    response = requests.post(get_bitly_link, headers=headers, json=payload)
-    response.raise_for_status()
-    bitlink = response.json()['link']
-    logging.info(response.status_code)
-    parsed_bitlink = urlparse(bitlink)
-    logging.info(parsed_bitlink)
-    bitlink = f'{parsed_bitlink.netloc}{parsed_bitlink.path}'
+    parsed_url = urlparse(url)
+    bitlink = f'{parsed_url.netloc}{parsed_url.path}'
+    get_clicks_count_url = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'.format(bitlink=bitlink)
 
-    get_clicks_count_link = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'.format(bitlink=bitlink)
-    response = requests.get(get_clicks_count_link, headers=headers)
-    logging.info(get_clicks_count_link)
-    pprint.pprint(response)
+    response = requests.get(get_clicks_count_url, headers=headers)
     response.raise_for_status()
-    logging.info(response.status_code)
-    
-    count = response.json()['total_clicks']
-    
-    return count
+    clicks = response.json()['total_clicks']
+    return clicks
 
 
 def main():
-    logging.basicConfig(level=logging.WARNING)
-    get_user_url = 'https://api-ssl.bitly.com/v4/user'
-    #long_url = 'http://google.com'
-    long_url = input('Введите ссылку для сокращениея: ')
-    
-    try:
-        print('Битлинк', shorten_link(BITLY_ACCESS_TOKEN, long_url))
-    except requests.exceptions.HTTPError:
-        print('Был введен неверный URL')
-    
-    try:
-        print(clicks_count(BITLY_ACCESS_TOKEN, long_url))
-    except requests.exceptions.HTTPError:
-        print('Ошибка при получении кол-ва кликов')
-    
-    
-    #logging.info(response)
-    #pprint.pprint(response.json())
+    url = input('Введите ссылку: ')
+    token = dotenv_values('.env')['BITLY_ACCESS_TOKEN']
 
-
+    if is_bitlink(token, url):
+        try:
+            print(f'Кол-во кликов по ссылке {url}: {clicks_count(token, url)}')
+        except requests.exceptions.HTTPError:
+            print('Ошибка при получении кол-ва кликов.')
+    else:
+        try:
+            print('Битлинк: ', shorten_link(token, url))
+        except requests.exceptions.HTTPError:
+            print('Введенный URL невозможно сократить.')
 
 
 if __name__ == '__main__':
